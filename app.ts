4428e2218846616951b570a71ae02a294b37b05e
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2020-12-05 18:08:18 -0300

    Algumas configs

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './src/router';
+import router from './src/routes';
 import logger from './src/services/logger';
 
 const app = express();
@@ -8,4 +8,7 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => logger.info("Api running on port " + port));
+app.listen(port, () => {
+  console.log("Api running on port " + port);
+  logger.info("Api running on port " + port);
+});
